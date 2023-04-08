969c9b4a3e8c61f6be488c406ff38a623044f1a2
aliogorobinna <aliogor7509@yahoo.com>
2023-04-08 02:30:00 +0200

    region updated

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 variable "region" {
     description = "Region in which AWS Resources will be created example us-east-1"
     type = string
-    #default = "us-east-2" I declared a variable for the provider without giving it default 
+    default = "us-east-2" #I declared a variable for the provider without giving it default 
     # value, the user will have to pass in the region they want the resources to be created at runtime "us-east-1"
 
   
